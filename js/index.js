9eeb2e8ff7c67d15f3c7971e8aea61e89dee33b9
seb99seb <sebvangpedersen@hotmail.dk>
2022-11-02 12:47:12 +0100

    første 15min

--- js/index.js
+++ js/index.js
@@ -1,26 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            word: "ok",
-            words: [],
-            Message: ""
+            number1: 0,
+            number2: 0,
+            result: 0
         }
     },
     computed:{
-        wordDisplay(){
-            return `${this.words+" "}`
-        }
+        
     },
-    methods: {
-        // add here
-        Save(){
-            this.words.push(this.word)
-        },
-        Show(){
-            //ok
-        },
-        Clear(){
-            this.words=[]
+    methods:{
+        calculate(){
+            this.result = this.number1 + this.number2
         }
     }
-})
+}).mount("#app")
